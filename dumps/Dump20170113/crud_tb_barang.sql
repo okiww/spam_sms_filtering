f885c3e6104de21a5b891c416d391c106e001154
mr-robot@mr-robot <mr-robot@mr-robot>
2017-01-13 00:57:42 +0700

    data dumps

---
+++ dumps/Dump20170113/crud_tb_barang.sql
@@ -0,0 +1,59 @@
+CREATE DATABASE  IF NOT EXISTS `crud` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `crud`;
+-- MySQL dump 10.13  Distrib 5.5.53, for debian-linux-gnu (x86_64)
+--
+-- Host: 127.0.0.1    Database: crud
+-- ------------------------------------------------------
+-- Server version	5.5.53-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tb_barang`
+--
+
+DROP TABLE IF EXISTS `tb_barang`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_barang` (
+  `kode_brg` int(5) NOT NULL AUTO_INCREMENT,
+  `barcode` varchar(20) NOT NULL,
+  `nama_brg` varchar(150) NOT NULL,
+  `harga_brg` int(10) NOT NULL,
+  `keterangan` tinytext NOT NULL,
+  `jenis` varchar(30) DEFAULT NULL,
+  `satuan` varchar(30) DEFAULT NULL,
+  `stok_brg` int(5) DEFAULT NULL,
+  PRIMARY KEY (`kode_brg`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_barang`
+--
+
+LOCK TABLES `tb_barang` WRITE;
+/*!40000 ALTER TABLE `tb_barang` DISABLE KEYS */;
+INSERT INTO `tb_barang` VALUES (3,'200','barang',2000,'hamncjanjacdc','Papan','kilo',20);
+/*!40000 ALTER TABLE `tb_barang` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-01-13  0:55:26
